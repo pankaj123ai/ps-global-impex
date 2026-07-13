1445f83335c32406d3d84b769c22b38b392729e5
pankaj123ai <pankajsharma13816@gmail.com>
2026-07-13 15:28:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-// PS Global Impex Website JavaScript
 
-console.log("Welcome to PS Global Impex!");
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    const button = document.querySelector(".button");
-
-    if(button){
-
-        button.addEventListener("click", function(){
-
-            alert("Thank you for contacting PS Global Impex. We will get back to you soon.");
-
-        });
-
-    }
-
-});
